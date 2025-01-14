cca11e3753d17b2b0efa4c6b8cd5efeee5ad9902
Amar Younis <ayounis.work@gmail.com>
2025-01-14 01:48:36 +0000

    fixed merge errors

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia';
 import App from './App.vue'
 import router from './router'
-import { createPinia } from 'pinia';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
 
 const app = createApp(App)
 const pinia = createPinia();
